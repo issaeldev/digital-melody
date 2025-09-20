dfcdcaa6f062e84ed0c866f01ad22bb0216c4e44
issaeldev <rocketfx7@gmail.com>
2025-09-19 20:02:25 -0500

    Refactor App and Navbar components for improved routing and layout; update styles and assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Colecciones from './pages/Colecciones'
+import Nosotros from './pages/Nosotros'
+import Contacto from './pages/Contacto'
 import Producto from './pages/Producto'
+import Carrito from './pages/Carrito'
+import Login from './pages/Login'
+import RealizarOrden from './pages/RealizarOrden'
+import Ordenes from './pages/Ordenes'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9xw]'>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
 
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Landing />} />
         <Route path='/colecciones' element={<Colecciones />} />        
         <Route path='/nosotros' element={<Nosotros />} />
         <Route path='/contacto' element={<Contacto />} />
@@ -18,7 +26,6 @@ const App = () => {
         <Route path='login' element={<Login/>}/>
         <Route path='realizar-orden' element={<RealizarOrden/>}/>
         <Route path='ordenes' element={<Ordenes/>}/>
-        <Route path='*' element={<NotFound/>}/>
        </Routes>
 
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { assets } from '../assets/frontend_assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} alt="logo-digital-melody" className='w-36'/>
+      <img src={assets.logo} alt="logo-digital-melody" className='w-36' />
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-        <NavLink className='flex flex-col items-center gap-1'>
-            <p>Home</p>    
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
+          <p>home</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/colecciones' className='flex flex-col items-center gap-1'>
+          <p>colecciones</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/nosotros' className='flex flex-col items-center gap-1'>
+          <p>nosotros</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/contacto' className='flex flex-col items-center gap-1'>
+          <p>contactanos</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
       </ul>
+
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="icono-buscar" />
+
+        <div className='relative group'>
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="icono-perfil" />
+          <div className='hidden group-hover:block absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>perfil</p>
+              <p className='cursor-pointer hover:text-black'>órdenes</p>
+              <p className='cursor-pointer hover:text-black'>salir</p>
+            </div>
+          </div>
+        </div>
+        <Link to='/carrito' className='relative'>
+          <img src={assets.cart_icon} className='w-5 min-w-5' alt="icono-carrito" />
+          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+      </div>
     </div>
   )
 }
